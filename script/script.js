c96b24d76498248db6a821cee97d97e20c7b0a8a
chococo3827 <105043732+chococo3827@users.noreply.github.com>
2023-08-11 21:39:18 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,18 +3,30 @@ $(function(){
     var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       effect: "fade",
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+      mousewheel: true,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
       autoplay:{
-        // delay: 1000
+        delay: 5000
+      },
+      on:{
+        reachEnd: function(){
+            swiper.mousewheel.disable();
+        }
       }
     });
+    window.addEventListener('wheel', function(event){
+        if(event.deltaY < 0){
+            swiper.mousewheel.enable();
+        }
+        else if(event.deltaY > 0){}
+    })
 })
 
 // guide info 스크롤시 나타내기
@@ -26,7 +38,7 @@ $(window).on('scroll resize', function(){
     guideList();
 
     function guideInfo(){
-        if(scrollPos > 2200){
+        if(scrollPos > 2150){
             $('.guide-info .text').addClass('on');
         }
         else{
@@ -39,28 +51,29 @@ $(window).on('scroll resize', function(){
 
     function guideList(){
         $('section.guide .inner .guide-list li a').removeClass('on');
-        if(scrollPos > 2200){
+        if(scrollPos > 2150){
             $('section.guide .inner .guide-list li a').removeClass('on');
             $('section.guide .inner .guide-list li:eq(0) a').addClass('on');
         }
-        if(scrollPos > 2600){
+        if(scrollPos > 2550){
             $('section.guide .inner .guide-list li a').removeClass('on');
             $('section.guide .inner .guide-list li:eq(1) a').addClass('on');
         }
-        if(scrollPos > 3000){
+        if(scrollPos > 2950){
             $('section.guide .inner .guide-list li a').removeClass('on');
             $('section.guide .inner .guide-list li:eq(2) a').addClass('on');
         }
-        if(scrollPos > 3400){
+        if(scrollPos > 3350){
             $('section.guide .inner .guide-list li a').removeClass('on');
             $('section.guide .inner .guide-list li:eq(3) a').addClass('on');
         }
-        if(scrollPos > 3800){
+        if(scrollPos > 3750){
             $('section.guide .inner .guide-list li a').removeClass('on');
             $('section.guide .inner .guide-list li:eq(4) a').addClass('on');
         }
     }
 })
+
 // section event hover
 $(function(){
     $('section.event .inner .event-list li a').hover(
@@ -85,6 +98,7 @@ $(function(){
     });
 })
 
+// 메뉴바 나타내기
 $(function(){
     $('header .inner .gnbBtn label').on('click', function(){
         $('nav.gnb').toggleClass('on');
@@ -92,4 +106,30 @@ $(function(){
     $('.contents').on('click', function(){
         $('nav.gnb').removeClass('on');
     })
+})
+
+// top 버튼 나타내기, 아래로 스크롤 시 header 축소
+$(window).scroll(function(){
+    if($(this).scrollTop() > 200){
+        $('#top').addClass('on');
+        $('header').addClass('on');
+        $('nav.gnb').addClass('active');
+    }
+    else{
+        $('#top').removeClass('on');
+        $('header').removeClass('on');
+        $('nav.gnb').removeClass('active');
+    }
+})
+
+// scrolla 이벤트
+$(function(){
+    $('.animate').scrolla({
+        mobile: true,
+        once: false
+    })
+})
+// 텍스트 애니메이션
+$(function(){
+    Splitting();
 })
